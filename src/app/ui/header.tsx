433a59a202ec069ad50c4bd0c231145033319568
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-11-29 23:04:43 -0800

    :zap: Add Link component for header menu

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex">
-      <Image src='/logo.png' width={400} height={50} alt="Logo | Madison Beer" />
+      <Link href="/">
+        <Image src='/logo.png' width={400} height={50} alt="Logo | Madison Beer" />
+      </Link>
       <ul className="flex">
         <li>Music</li>
         <li>Videos</li>
         <li>LIVE</li>
         <li>Photos</li>
         <li>News</li>
-        <li>About</li>
+        <li><Link href="/about">About</Link></li>
       </ul>
     </header>
   )
