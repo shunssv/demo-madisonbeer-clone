69eb005c08ad3afa765ec0ac2cc16ab9f48d0d77
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-12-03 23:35:20 -0800

    :bento: Add hero images for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='hover:text-yellow-400'>Madison site Clone</p>
+    <>
+      <p className="hover:text-yellow-400">Madison site Clone</p>
+      <Image src="/home/hero-1.jpg" width={1000} height={500} alt="madison beer tour" className="mx-auto" />
+    </>
   )
 }
