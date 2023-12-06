68539ba8d70587e9f34162bf23084693c89ef5b1
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-12-05 23:57:43 -0800

    :bento: Add images for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
-    <p className=" text-7xl">Madison Beer is a platinum-selling recording artist, songwriter, producer, and author</p>
+    <section className="">
+      <div className="flex ">
+        <h1 className=" text-7xl">Madison Beer is a platinum-selling recording artist, songwriter, producer, and author</h1>
+        <Image src="/about/profile-1.jpg" width={1200} height={1300} alt="profile | madison beer"/>
+      </div>
+
+      <Image src="/about/profile-2.jpg" width={1200} height={1100} alt="profile | madison beer"/>
+    </section>
   )
 }
